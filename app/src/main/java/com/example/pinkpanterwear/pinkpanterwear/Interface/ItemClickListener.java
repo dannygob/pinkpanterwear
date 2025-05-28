217cf0b66a146d2b895813b77842a4cb1fac1cca
tardes <dannygomezb@gmail.com>
2025-05-28 20:06:54 +0200

    init commit

--- app/src/main/java/com/example/pinkpanterwear/pinkpanterwear/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.pinkpanterwear.pinkpanterwear.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
--- app/src/main/java/com/example/pinkpanterwear/pinkpanterwear/Model/SliderItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.slickkwear.Model
-
-class SliderItem {
-    @JvmField
-    var description: String? = null
-
-    @JvmField
-    var imageUrl: String? = null
-}
