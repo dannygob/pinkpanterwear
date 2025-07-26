45a6cc23cfde8e9be2c1d7749919335565686ae0
Superhola <dannygomeb@gmail.com>
2025-07-26 12:43:51 +0200

    add fix 5
    Signed-off-by: Superhola <dannygomeb@gmail.com>

--- app/src/main/java/com/example/pink/fragment/UserAccountFragment.kt
+++ app/src/main/java/com/example/pink/fragment/UserAccountFragment.kt
@@ -9,57 +9,39 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.pink.R
+import com.example.pink.activities.AdminHomeActivity
 import com.example.pink.activities.LoginActivity
 import com.example.pink.activities.RegisterActivity
-import com.example.pink.activities.adminHomeActivity
 import com.google.android.material.textview.MaterialTextView
 
 class UserAccountFragment : Fragment() {
-    private var user_account_admin: MaterialTextView? = null
-    private var register_link_btn: Button? = null
+    private var userAccountAdmin: MaterialTextView? = null
+    private var registerLinkBtn: Button? = null
+    private var loginLinkBtn: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_user_account, container, false)
 
-        user_account_admin = view.findViewById<View?>(R.id.user_account_admin) as MaterialTextView
+        userAccountAdmin = view.findViewById(R.id.user_account_admin)
+        registerLinkBtn = view.findViewById(R.id.user_register_link_btn)
+        loginLinkBtn = view.findViewById(R.id.account_my_wishlist)
 
-        register_link_btn = view.findViewById<View?>(R.id.user_register_link_btn) as Button
-        register_link_btn!!.setOnClickListener(
-            object : View.OnClickListener {
-                override fun onClick(view: View?) {
-                    val intent = Intent(context, RegisterActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-        )
+        registerLinkBtn?.setOnClickListener {
+            startActivity(Intent(context, RegisterActivity::class.java))
+        }
 
+        loginLinkBtn?.setOnClickListener {
+            startActivity(Intent(context, LoginActivity::class.java))
+        }
 
-        val verify_link_btn = view.findViewById<View?>(R.id.account_my_wishlist) as TextView
-        verify_link_btn.setOnClickListener(
-            object : View.OnClickListener {
-                override fun onClick(view: View?) {
-                    val intent = Intent(context, LoginActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-        )
-
-
-
-        user_account_admin!!.setOnClickListener(
-            object : View.OnClickListener {
-                override fun onClick(view: View?) {
-                    val intent = Intent(context, adminHomeActivity::class.java)
-                    startActivity(intent)
-                }
-            }
-        )
-
+        userAccountAdmin?.setOnClickListener {
+            startActivity(Intent(context, AdminHomeActivity::class.java))
+        }
 
         return view
     }
+}
 }
